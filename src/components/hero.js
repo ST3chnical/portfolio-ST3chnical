a00e34567dd856f81afb729122cfb82675830d38
ST3chnical <samuelmercado7b@gmail.com>
2023-09-06 15:03:48 -0500

    Fix description

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
     return (
         <section className="hero">
             <div className="hero-left">
-                <h1>Hi, I am Samuel a Software Developer.</h1>
+                <h1>Hi, I'm Samuel, a Software Developer.</h1>
                 <p className="paragraph">And this is my portfolio.</p>
                 <button className="email-button"><img src="/svg/mail.svg" alt="mail" />samuelmercado7b@gmail.com</button>
             </div>
