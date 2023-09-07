9915dae8e573bfd8fe0938728af2c532eab78dc3
ST3chnical <samuelmercado7b@gmail.com>
2023-09-06 22:45:12 -0500

    Added responsive design and menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/header.css';
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
+
     return (
         <header>
             <nav className='navbar-left'>
                 <span className='logo'><img src="/svg/logo.svg" alt="logo"/></span>
             </nav>
             <nav className='navbar-right'>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#about-me">About me</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                <div className={`menu-toggle ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                    <img src="/svg/menu-dots.svg" alt="Menu Icon" />
+                </div>
+                <ul className={`menu ${isMenuOpen ? 'open' : ''}`}>
+                    <li><a href="#" onClick={closeMenu}>Home</a></li>
+                    <li><a href="#about-me" onClick={closeMenu}>About me</a></li>
+                    <li><a href="#projects" onClick={closeMenu}>Projects</a></li>
+                    <li><a href="#contact" onClick={closeMenu}>Contact</a></li>
                 </ul>
             </nav>
         </header>
     );
 }
 
-export {Header};
+export { Header };
